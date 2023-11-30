38f5eb909a2fa370262f41096e534837c726aa29
arcan <arcan@ARCANSPRO9>
2023-11-29 22:23:40 -0500

    Add project files.

---
+++ Dotnet1/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dotnet1
+{
+    internal class Class1
+    {
+        // This class contains the methid to calculate the tax.
+        // We will calculate tax deduction for CPP and EI.
+        Console.WriteLine("Enter your income (or 'q' to quit): ");
+            string input = Console.ReadLine();
+
+    }
+}
---
+++ Dotnet1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+using System;
+using System.Runtime.CompilerServices;
+
+namespace TaxCalculator
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            bool runAgain = true;
+            while(runAgain)
+            { runAgain = runTaxCalculator(); }
+            
+            }
+        Console.WriteLine("Press any key to exit...");
+        }   
+private static bool runTaxCalculator()
+    {
+    Console.WriteLine("Enter your income (or 'q' to quit): ");
+    string input = Console.ReadLine();
+    if (input == "q")
+        {
+        return false;
+    }
+    else
+        {
+        double income = Convert.ToDouble(input);
+        double cpp = calculateCPP(income);
+        double ei = calculateEI(income);
+        double tax = calculateTax(income);
+        double netIncome = income - cpp - ei - tax;
+        Console.WriteLine("Your net income is: " + netIncome);
+        return true;
+    }
+    }
+}
